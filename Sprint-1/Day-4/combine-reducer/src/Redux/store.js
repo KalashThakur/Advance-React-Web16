a6d2688e8c3d25a0fba63171a7ad4009aa96e918
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-06-27 00:38:28 +0530

    updated store

--- Sprint-1/Day-4/combine-reducer/src/Redux/store.js
+++ Sprint-1/Day-4/combine-reducer/src/Redux/store.js
@@ -1,4 +1,4 @@
-import { legacy_createStore, combineReducers } from "redux";
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import { authReducer } from "./auth/reducer";
 import { todoReducer } from "./todos/reducer";
 
@@ -7,6 +7,24 @@ import { todoReducer } from "./todos/reducer";
 const rootReducer = combineReducers( {
     auth: authReducer,
     app: todoReducer
-} )
+} );
 
-export const store = legacy_createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION_ || window.__REDUX_DEVTOOLS_EXTENSION__());
+const logger = (state) => (next) => (action) => {
+    console.log("dispatching action", action, next, state);
+    const val = next(action);
+    console.log("exciting logger");
+    return val
+}
+
+const logger2 = (state) => (next) => (action) => {
+    console.log("dispatching action from logger 2", action, next, state);
+    const val = next(action);
+    console.log("exciting logger2");
+    return val
+}
+
+export const store = legacy_createStore(
+    rootReducer, 
+    applyMiddleware(logger, logger2)
+    // window.__REDUX_DEVTOOLS_EXTENSION_ || window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
